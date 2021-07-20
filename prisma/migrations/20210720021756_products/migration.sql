9f400cd41a3db195bd5b4e4c4d85d1f5615eee48
João Eliandro Germano <eliandrogermano@gmail.com>
2021-07-20 00:07:26 -0300

    Added prisma migrations

---
+++ prisma/migrations/20210720021756_products/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
